59e63dfcba406925ef9684eaac9a3c9aded8c28f
David Asensio Cañas <david.asensio@icloud.com>
2017-04-05 23:28:45 +0200

    Adding a npm task and a husk git hook linked to it, to prevent future linter warnings.

---
+++ tasks/check_linter_rules.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const stylelint = require('stylelint');
+
+const linter_options = {
+    configFile: '.stylelintrc',
+    files: '**/*.scss',
+    formatter: 'verbose',
+    ignoreDisables: false,
+    syntax: 'scss'
+};
+
+stylelint
+    .lint(linter_options)
+    .then((analysis) => {
+        console.log(analysis.output);
+
+        for (let result of analysis.results) {
+            if (result.warnings.length) {
+                process.exit(1);
+            }
+        }
+
+        process.exit(0);
+    })
+    .catch((error) => {
+        console.error(error);
+
+        process.exit(1);
+    });
